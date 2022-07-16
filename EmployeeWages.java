c756b16c7f17e0eb55e7f3a74356afd0aefab969
Harish mahur <hmahur20@gmail.com>
2022-07-16 13:29:51 +0530

    modified:employee dily wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,16 +5,25 @@ public class EmployeeWages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Welcome to employee wage computation");
+
+		int wagePerHour = 20;
+		int numberOfHourPerDay = 8;
+
 		int attandance = 1;
 		double randomCheck = Math.floor(Math.random() * 10) % 2;
 		if (attandance == randomCheck) {
-			System.out.println("Employee is present");
-		} else {
-			System.out.println("Employee is absent");
+
+			if (numberOfHourPerDay == 8) {
+				System.out.println("Employee is working Full day");
+				int wageOfTheDay = numberOfHourPerDay * wagePerHour;
+				System.out.println("Daily wage of the employee is" + " " + wageOfTheDay);
+
+			} else {
+				System.out.println("Employee is not working today.");
+			}
 		}
-	}
-
 
 	}
+}
 
 
