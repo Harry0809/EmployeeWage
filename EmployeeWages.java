6ac302ea5576c225f981e3ee0f3211daaf0d57f9
Harish mahur <hmahur20@gmail.com>
2022-07-16 12:58:24 +0530

    welcome to employee wages

---
+++ EmployeeWages.java
@@ -0,0 +1,11 @@
+package com.employee.wages.java;
+
+public class EmployeeWages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Welcome to employee wage computation");
+
+	}
+
+}
