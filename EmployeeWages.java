c920fd95f6543809b3285225976fedda95c5acbc
Harish mahur <hmahur20@gmail.com>
2022-07-16 14:02:17 +0530

    modifiied:monthly wage of employee

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -6,29 +6,32 @@ public class EmployeeWages {
 		// TODO Auto-generated method stub
 		System.out.println(" Welcome to employee wage computation");
 
-		int wagePerHour = 20;
-		int numberOfHourPerFullDay = 8;
-		int numberOfHourPerPartDay = 4;
-		int wageOfTheDay = 0;
-
-		int employeeAttendance = (int) (Math.floor(Math.random() * 10)) % 2;
+		int wagePerHour=20;
+		int numberOfHoursPerFullDay=8;
+		int numberOfHoursPerPartDay=4;
+		int numberOfWorkingDaysInMonth=20;
+		int wageOfTheMonth=0;
+		
+int employeeAttendance = (int) (Math.floor(Math.random() * 10))%2;
+		
 		switch (employeeAttendance) {
-
+		
 		case 1:
 			System.out.println("Emoloyee is working full day");
-			wageOfTheDay = numberOfHourPerFullDay * wagePerHour;
-			System.out.println("full day wage of the employee is" + " " + wageOfTheDay);
+		  wageOfTheMonth = numberOfHoursPerFullDay *  wagePerHour *  numberOfWorkingDaysInMonth ;
+			System.out.println("monthly wage of the employee is" +  " " + wageOfTheMonth);
 			break;
-
+			
 		case 2:
 			System.out.println("Employee is working part day");
-			wageOfTheDay = numberOfHourPerPartDay * wagePerHour;
-			System.out.println("Part time  wage of employee is" + " " + wageOfTheDay);
-			break;
-
+		   wageOfTheMonth = numberOfHoursPerPartDay *  wagePerHour * numberOfWorkingDaysInMonth ;
+		   System.out.println("montghly "
+		   		+ "wage of employee is" + wageOfTheMonth);
+		   break;
 		default:
-			System.out.println("employee is not working today");
+			System.out.println("employee is absent");
 			break;
+			
 
 		}
 
