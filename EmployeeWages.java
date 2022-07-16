3a72790b1e3bd65f73852059530b199660a49173
Harish mahur <hmahur20@gmail.com>
2022-07-16 14:07:32 +0530

    modifiied:wage of employee til a condition

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -6,34 +6,42 @@ public class EmployeeWages {
 		// TODO Auto-generated method stub
 		System.out.println(" Welcome to employee wage computation");
 
-		int wagePerHour=20;
-		int numberOfHoursPerFullDay=8;
-		int numberOfHoursPerPartDay=4;
-		int numberOfWorkingDaysInMonth=20;
-		int wageOfTheMonth=0;
-		
-int employeeAttendance = (int) (Math.floor(Math.random() * 10))%2;
-		
-		switch (employeeAttendance) {
-		
-		case 1:
-			System.out.println("Emoloyee is working full day");
-		  wageOfTheMonth = numberOfHoursPerFullDay *  wagePerHour *  numberOfWorkingDaysInMonth ;
-			System.out.println("monthly wage of the employee is" +  " " + wageOfTheMonth);
-			break;
-			
-		case 2:
-			System.out.println("Employee is working part day");
-		   wageOfTheMonth = numberOfHoursPerPartDay *  wagePerHour * numberOfWorkingDaysInMonth ;
-		   System.out.println("montghly "
-		   		+ "wage of employee is" + wageOfTheMonth);
-		   break;
-		default:
-			System.out.println("employee is absent");
-			break;
-			
+		int numberOfHoursPerPartDay = 4;
+		int numberOfHoursPerDayFullDay = 8;
+		int wagePerHour = 20;
+		int maxWorkingDays = 20;
+		int maxWorkingHours = 100;
+		int totalDays=0;
+		int totalHours = 0;
+		int dailyWage = 0;
+		int totalWageOfMonth = 0;
+		int days = 0;
 
+		while ((totalHours <= maxWorkingHours) && (totalDays <= maxWorkingDays)) {
+			int employeeAttendance = (int) (Math.floor(Math.random() * 10)) % 2;
+
+			switch (employeeAttendance) {
+
+			case 1:
+				dailyWage = numberOfHoursPerDayFullDay * wagePerHour;
+				totalHours += numberOfHoursPerDayFullDay;
+				break;
+
+			case 2:
+				dailyWage = numberOfHoursPerPartDay * wagePerHour;
+				totalHours += numberOfHoursPerPartDay;
+				break;
+
+			default:
+				break;
+			}
+
+			totalWageOfMonth += dailyWage;
+			days++;
+		}
+		System.out.println("Total work hour of employee is" +" "+ totalHours + " " + "And total work days are" +" "+  days
+				 +" " + "so the total wage of employee is " +" " +totalWageOfMonth);
 		}
 
 	}
-}
+
