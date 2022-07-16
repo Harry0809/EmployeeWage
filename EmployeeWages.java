b81a7b13394cb05c7c22241675d9e61855806d2f
Harish mahur <hmahur20@gmail.com>
2022-07-16 13:34:24 +0530

    modified:employee working and wage

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -7,20 +7,30 @@ public class EmployeeWages {
 		System.out.println(" Welcome to employee wage computation");
 
 		int wagePerHour = 20;
-		int numberOfHourPerDay = 8;
-
-		int attandance = 1;
-		double randomCheck = Math.floor(Math.random() * 10) % 2;
-		if (attandance == randomCheck) {
-
-			if (numberOfHourPerDay == 8) {
-				System.out.println("Employee is working Full day");
-				int wageOfTheDay = numberOfHourPerDay * wagePerHour;
-				System.out.println("Daily wage of the employee is" + " " + wageOfTheDay);
-
-			} else {
-				System.out.println("Employee is not working today.");
-			}
+		int numberOfHourPerFullDay=8;
+		int numberOfHourPerPartDay=4;
+		int wageOfTheDay=0;
+		
+		int employeeAttendance = (int) (Math.floor(Math.random() * 10))%2;
+		
+		switch (employeeAttendance) {
+		
+		case 1:
+			System.out.println("Emoloyee is working full day");
+		  wageOfTheDay = numberOfHourPerFullDay *  wagePerHour ;
+			System.out.println("full day wage of the employee is" + " " + wageOfTheDay);
+			break;
+			
+		case 2:
+			System.out.println("Employee is working part day");
+		   wageOfTheDay = numberOfHourPerPartDay *  wagePerHour ;
+		   System.out.println("Part time  wage of employee is" + " " +  wageOfTheDay);
+		   break;
+		   
+		default:
+			System.out.println("employee is not working today");
+			break;
+			
 		}
 
 	}
