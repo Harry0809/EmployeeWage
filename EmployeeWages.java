024fc91e4f89eee6888e3b6e3f961c1beb197173
Harish mahur <hmahur20@gmail.com>
2022-07-16 13:17:14 +0530

    modified:employee present absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,7 +5,16 @@ public class EmployeeWages {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Welcome to employee wage computation");
+		int attandance = 1;
+		double randomCheck = Math.floor(Math.random() * 10) % 2;
+		if (attandance == randomCheck) {
+			System.out.println("Employee is present");
+		} else {
+			System.out.println("Employee is absent");
+		}
+	}
+
 
 	}
 
-}
+
